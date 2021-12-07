c253bd63583fec87c941a3659cd73d13b9340dc3
Kevin Quinn <kevin.quinn@totalserversolutions.com>
2021-12-07 15:54:17 -0500

    python day 7
    this is not an optimal solution

---
+++ 7/7.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from pprint import pprint
+from collections import defaultdict
+from statistics import median
+import sys
+
+with open('data.txt') as file:
+    for row in file:
+        positions = [int(n) for n in row.rstrip().split(',')]
+        continue
+
+# arithmetic sum formula (1 + 2 + 3 ... n)
+def fuel_cost(n):
+    return (n / 2) * (1 + n)
+
+# there is probably a math magic trick somewhere to make this faster
+# brute force for now..
+
+# part 1
+lowest = 999999999999999999999
+for guess in range(min(positions), max(positions)):
+    cost = 0
+    for n in positions:
+        cost += abs(n - guess)
+    if cost < lowest:
+        lowest = cost
+
+pprint(lowest)
+
+# part 2
+lowest = 999999999999999999999
+for guess in range(min(positions), max(positions)):
+    cost = 0
+    for n in positions:
+        cost += fuel_cost(abs(n - guess))
+    if cost < lowest:
+        lowest = cost
+
+pprint(lowest)
